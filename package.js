658ca3f1e3f17c88228daf4bf754c947ae8ba83e
Ivan S Glazunov <ivansglazunov@gmail.com>
2016-02-17 05:43:06 +0300

    New dependecies logic.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ivansglazunov:shuttle',
-  version: '0.0.1',
+  version: '0.1.0',
   summary: 'Theory of trees in an atmosphere',
   git: 'https://github.com/ivansglazunov/meteor-shuttle.git',
   documentation: 'README.md'
@@ -12,29 +12,15 @@ Package.onUse(function(api) {
   api.use('ecmascript');
   api.use('mongo');
   api.use('accounts-base');
-
   api.use('templating');
-  api.use('meteorhacks:ssr@2.2.0');
-
-  api.use('matb33:collection-hooks@0.8.1');
-  api.use('lai:collection-extensions@0.2.1');
-  api.use('aldeed:collection2@2.8.0');
-  api.use('brettle:accounts-deluxe@0.2.2');
-  api.use('easy:search@2.0.8');
   
-  api.use('ivansglazunov:refs@0.0.1');
-  api.use('ivansglazunov:trees@1.1.3');
+  api.use('brettle:accounts-deluxe@0.2.2');
+  
+  api.use('ivansglazunov:shuttle-rights@0.0.1');
+  api.use('ivansglazunov:shuttle-fetching@0.0.1');
+  api.use('ivansglazunov:shuttle-posts@0.0.1');
   
   api.addFiles([
-    'shuttle.js',
-    'inserted.js',
-    'subjects.js',
-    'joining.js',
-    'rights.js',
-    'unused.js',
-    'owning.js',
-    'fetching.js'
+    'shuttle.js'
   ]);
-
-  api.export('Shuttle');
 });
--- shuttle.js
+++ shuttle.js
@@ -1,90 +1,34 @@
-Shuttle = {};
-
-// Shuttle.get(tree, object, subject) => Cursor
-Shuttle.get = function(tree, object, subject) {
-	if (!subject) {
-		var user = Meteor.user();
-		if (user) var subject = user;
-		else var subject = Meteor.users.findOne('guest');
-	}
-	if (!subject) {
-		var subject = Meteor.users._transform({ _id: 'guest' });
-	}
-	var subjects = Shuttle.Subjects.find(subject.Ref('_source')).fetch();
-	var $or = [subject.Ref('_target')];
-	for (var s in subjects) {
-		$or.push(subjects[s].target().Ref('_target'));
-	}
-	var query = object.Ref('_source');
-	query['$or'] = $or;
-	return tree.find(query);
-};
-
-// Shuttle.can(tree, object, subject) => Boolean
-Shuttle.can = function(tree, object, subject) {
-	if (!object) return false;
-	return !!Shuttle.get(tree, object, subject).count();
-};
-
-CollectionExtensions.addExtension(function (name, options) {
-	this.attachRefs();
-});
-
 if (Meteor.isClient) {
-	Template.registerHelper('Shuttle', function(target) {
-		return Shuttle;
-	});
+	Template.registerHelper('Shuttle', function() { return Shuttle; });
+	Template.registerHelper('Meteor', function() { return Meteor; });
 }
 
 if (Meteor.isServer) {
-	Mongo.Collection.prototype.attachSecure = function(Tree, field) {
-		var Collection = this;
-	
-		var schema = {};
-		schema[field] = { type: [Refs.Schema], optional: true };
-		Shuttle.Posts.attachSchema(schema);
-	
-		Collection.deny({
-			insert: function (userId, document) {
-				if (field in document)
-					throw new Meteor.Error('Users can not insert document with secure fields.');
-			},
-			update: function (userId, document, fieldNames, modifier) {
-				if (lodash.includes(fieldNames, field))
-					throw new Meteor.Error('Users can not update secure fields.');
-			}
-		});
+	if (!Meteor.users.findOne({ _id: 'guest' }))
+		Meteor.users.insert({ _id: 'guest' });
 		
-		this.mirrorTargetsFromTree(Tree, field);
-	
-		Collection.before.find(function (userId, selector, options) {
-			if (!userId) userId = 'guest';
-			if (!('$and' in selector)) selector.$and = [];
-			var $or = [];
-			// var subjects = Shuttle.Subjects.find({ '_source.collection': Meteor.users._name, '_source.id': userId }).fetch();
-			// for (var s in subjects) {
-			// 	var push = {};
-			// 	push[field+'.collection'] = subjects[s]._target.collection;
-			// 	push[field+'.id'] = subjects[s]._target.id;
-			// 	$or.push(push);
-			// }
-			var push = {};
-			push[field+'.collection'] = Meteor.users._name;
-			push[field+'.id'] = userId;
-			$or.push(push);
-			selector.$and.push({ $or: $or });
-		});
-	};
-}
-
-Template.registerHelper('Shuttle', function() { return Shuttle; });
-Template.registerHelper('Meteor', function() { return Meteor; });
-
-if (Meteor.isServer) {
-	var guest = Meteor.users.findOne({ _id: 'guest' });
-	if (!guest) {
-		Meteor.users.insert({
-			_id: 'guest'
-		})
-	}
+	if (!Meteor.users.findOne({ _id: 'user' }))
+		Meteor.users.insert({ _id: 'user' });
+		
+	if (!Meteor.users.findOne({ _id: 'registred' }))
+		Meteor.users.insert({ _id: 'registred' });
+		
+	Meteor.users.after.insert(function(userId, _user) {
+		var user = Meteor.users._transform(_user);
+		Shuttle.Subjects.link(user, Meteor.users.findOne('user'));
+		Shuttle.Subjects.link(user, Meteor.users.findOne('guest'));
+	});
+	Accounts.onLogin(function(login) {
+		if (login.type == 'resume') {
+			var user = Meteor.users._transform(login.user);
+			var subject = Shuttle.Subjects.findOne(lodash.merge(
+				Meteor.users.findOne('guest').Ref('_target'), user.Ref('_source')
+			));
+			if (subject) Shuttle.Subjects.remove(subject._id);
+			var subject = Shuttle.Subjects.findOne(lodash.merge(
+				Meteor.users.findOne('registred').Ref('_target'), user.Ref('_source')
+			));
+			if (!subject) Shuttle.Subjects.link(user, Meteor.users.findOne('registred'));
+		}
+	});
 }
